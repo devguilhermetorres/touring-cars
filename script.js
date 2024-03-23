029f9cb132fdcbf35dc59467ab0ae882ff397de9
devguilhermetorres <guitorres2001@outlook.com>
2024-03-23 10:46:13 -0300

    responsividade

--- script.js
+++ script.js
@@ -1,34 +1,24 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
-    let acessarestoque2 = document.querySelector('#estoque2');
-    const viewportWidth = window.innerWidth;
-    var w = window, 
-    d = document, 
-    e = d.documentElement, 
-    g = d.getElementsByTagName('body')[0], 
-    x = w.innerWidth || e.clientWidth || g.clientWidth;
+    let menuMobileicon = document.querySelector('.mobile-menu-icon');
+    var body = document.getElementById("body");
 
-
+ 
 
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "imgs/menublack.png";
-        acessarestoque2.classList.remove('acessarestoque3');
-        acessarestoque2.classList.add('acessarestoque2');
+        body.style.position = "relative";
+        menuMobileicon.style.position= "relative";
+        menuMobileicon.classList.remove('menu-open');
         
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "imgs/closeblack.png";
-        acessarestoque2.classList.remove('acessarestoque2');
-        acessarestoque2.classList.add('acessarestoque3');
-    }
+        body.style.overflowY = "scroll";
+        menuMobileicon.style.position= "fixed";
 
-    if (x > 1000) {
-        acessarestoque2.style.display = "none"
-    }
-
-    else {
-        
+        menuMobileicon.classList.add('menu-open');
     }
 };
 
