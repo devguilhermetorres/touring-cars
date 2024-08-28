95b3ec3f73180b54a8c510a056de71466ba5ef8a
devguilhermetorres <guitorres2001@outlook.com>
2024-08-28 10:58:14 -0300

    formatting phone number insertion in forms

--- script.js
+++ script.js
@@ -65,3 +65,16 @@ function validarRegister(){
     
 };
 
+document.getElementById('numero').addEventListener('input', function (e) {
+    let input = e.target.value;
+    input = input.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+
+    if (input.length <= 10) {
+        input = input.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3');
+    } else {
+        input = input.replace(/(\d{2})(\d{5})(\d{0,4})/, '($1) $2-$3');
+    }
+
+    e.target.value = input;
+});
+
