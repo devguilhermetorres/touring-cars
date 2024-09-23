1c16ff262004ee823bdc1c574ed76da3f4119c4e
devguilhermetorres <guitorres2001@outlook.com>
2024-09-23 08:08:40 -0300

    mobile menu adjustments

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+//form validate
+
 function validateRegister(){
-    var modeloVazio = modelo.value;
-    var nomeVazio = nome.value; 
+    var modelVazio = model.value;
+    var nameVazio = name.value; 
     var emailVazio = email.value;
-    var numeroVazio = numero.value;
+    var numberVazio = number.value;
     
-    if( !(modeloVazio == "") && !(nomeVazio == "") && !(emailVazio == "") &&     !(numeroVazio == "") ){
+    if( !(modelVazio == "") && !(nameVazio == "") && !(emailVazio == "") &&     !(numberVazio == "") ){
     document.register.idButton.disabled=false;
     document.register.idButton.classList.remove("button");
     document.register.idButton.classList.add("buttonactive");
@@ -18,6 +20,8 @@ function validateRegister(){
     
 }
 
+//menu mobile
+
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     let menuMobileicon = document.querySelector('.mobile-menu-icon');
@@ -25,15 +29,16 @@ function menuShow() {
 
 
     if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.add('menu-closed');
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "imgs/icons8-menu.svg";
         body.style.position = "relative";
         menuMobileicon.style.position= "relative";
         body.style.overflow = "scroll";
-        menuMobileicon.classList.remove('menu-open');
         
     } else {
         menuMobile.classList.add('open');
+        menuMobile.classList.remove('menu-closed');
         document.querySelector('.icon').src = "imgs/icons8-close.svg";
         body.style.overflow = "hidden";
         menuMobileicon.style.position= "fixed";
@@ -62,9 +67,11 @@ function menuShowFilter() {
     }
 };
 
-document.getElementById('numero').addEventListener('input', function (e) {
+//number filter
+
+document.getElementById('number').addEventListener('input', function (e) {
     let input = e.target.value;
-    input = input.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    input = input.replace(/\D/g, ''); 
 
     if (input.length <= 10) {
         input = input.replace(/(\d{2})(\d{4})(\d{0,4})/, '($1) $2-$3');
@@ -74,6 +81,8 @@ document.getElementById('numero').addEventListener('input', function (e) {
 
     e.target.value = input;
 });
+
+//subscribe
 
 function validateSubscribe(){
     var emailVazio = Email.value;
@@ -90,4 +99,50 @@ function validateSubscribe(){
         document.subscribe.idButton.classList.add("buttonsubscribe");
     }
     
+}
+
+//filter stock
+
+function category(c){
+    var item = document.getElementById('item-'+c).innerHTML;
+    document.getElementsByTagName('input')[0].value = item;
+}
+
+function category1(c){
+    var item = document.getElementById('iitem-'+c).innerHTML;
+    document.getElementsByTagName('input')[1].value = item;
+}
+
+function dropDown(p){
+    var e = document.getElementsByClassName('dropDown')[0];
+    var d = ['block', 'none'];
+    e.style.display = d[p];   
+}
+
+function dropDown2(p){
+    var e = document.getElementsByClassName('dropDown2')[0];
+    var d = ['block', 'none'];
+    e.style.display = d[p];   
+}
+
+function category2(c){
+    var item2 = document.getElementById('item2-'+c).innerHTML;
+    document.getElementsByTagName('input')[10].value = item2;
+}
+
+function category3(c){
+    var item2 = document.getElementById('iitem2-'+c).innerHTML;
+    document.getElementsByTagName('input')[11].value = item2;
+}
+
+function dropDown3(p){
+    var e = document.getElementsByClassName('dropDown3')[0];
+    var d = ['block', 'none'];
+    e.style.display = d[p];   
+}
+
+function dropDown4(p){
+    var e = document.getElementsByClassName('dropDown4')[0];
+    var d = ['block', 'none'];
+    e.style.display = d[p];   
 }
